cf1e464826701b5d077918d91c37d58c8bc65171
Eduard Tesvintsev <tesvintsev@gmail.com>
2022-11-14 19:52:20 +0300

    added models and validations

---
+++ index.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+
+import UserModel from './models/User.js';
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:admin123@cluster0.ve62pya.mongodb.net/blog?retryWrites=true&w=majority',
+  )
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello Backend and NodeJS');
+});
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось зарегтсьртроваться',
+    });
+  }
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Server OK');
+});
